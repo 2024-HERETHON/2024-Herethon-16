77ceba9cba3a25fdc69b9654eda6637104ceba8b
SeYun <cindy21cx@gmail.com>
2024-07-04 11:26:17 +0900

    video views 수정

--- danjang_prj/videos/views.py
+++ danjang_prj/videos/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from portfolios.models import *
 from django.db.models import Q
+import random
 
 def video_list(request):
     videos = Video.objects.all()
-    
-    return render(request, "videos/video_list.html", {'videos' : videos})
+    random_videos = random.sample(list(videos), 10)
+    ranking_videos = Video.objects.order_by('-views')[:10]
+    new_videos = Video.objects.order_by('-created_at')[:10]
+    return render(request, "videos/video_list.html", {'videos' : videos, 'random_videos':random_videos, 'ranking_videos':ranking_videos, 'new_videos': new_videos})
 
 def video_detail(request, id):
     video = get_object_or_404(Video, id = id)
